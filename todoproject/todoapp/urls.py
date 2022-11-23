d9e5f0c5933caf77015c17c2c12be264f8ada884
MahimaUddaraju <105192490+MahimaUddaraju@users.noreply.github.com>
2022-11-23 14:47:40 +0530

    Delete todoproject directory

--- todoproject/todoapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-app_name = 'todoapp'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('todo/', views.todoappView, name='todo'),
-    path('addTodoItem/',views.addTodoView), 
-    path('deleteTodoItem/<int:i>/', views.deleteTodoView), 
-] 
